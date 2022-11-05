0b6d8f43090fdcf27eeec5d1cd7c79edb18bdef4
Drew <dongyoon.dev@gmail.com>
2022-11-05 14:34:36 +0900

    [2022.11.05] 1. add project file 2. add .gitignore file

---
+++ today-photo/today-photo/today_photoApp.swift
@@ -0,0 +1,17 @@
+//
+//  today_photoApp.swift
+//  today-photo
+//
+//  Created by 김동윤 on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct today_photoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
