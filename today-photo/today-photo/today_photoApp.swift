596fa3732464cae077697b4a6fdde8d094d635ed
Drew <dongyoon.dev@gmail.com>
2022-11-14 00:03:22 +0900

    setting

---
+++ today-photo/today-photo/today_photoApp.swift
@@ -0,0 +1,23 @@
+//
+//  today_photoApp.swift
+//  today-photo
+//
+//  Created by 김동윤 on 2022/11/05.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct today_photoApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
