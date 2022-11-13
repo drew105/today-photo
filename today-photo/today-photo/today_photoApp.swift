a7badacee2fea0e417cb24e38835706c12142161
Drew <dongyoon.dev@gmail.com>
2022-11-13 13:00:15 +0900

    feat: Firebase setting

--- today-photo/today-photo/today_photoApp.swift
+++ today-photo/today-photo/today_photoApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct today_photoApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
