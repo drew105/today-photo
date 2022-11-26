7679d475f22e1a86ab6061be6e768d9f94bd04f0
jjoyra <bunny7531@naver.com>
2022-11-26 13:45:43 +0900

    :heavy_plus_sign: [add]: user 정보 받아오기
    user 정보 받아오기

---
+++ today-photo/today-photo/View/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  today-photo
+//
+//  Created by 조희라 on 2022/11/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
